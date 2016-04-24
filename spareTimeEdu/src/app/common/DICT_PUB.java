8237d0bd0342dead983bbaa20c93ab814a022633
iamsharleen <iamsharleen@outlook.com>
2016-04-25 07:49:57 +0800

    2016.4.25

---
+++ spareTimeEdu/src/app/common/DICT_PUB.java
@@ -0,0 +1,26 @@
+package app.common;
+
+public class DICT_PUB {
+	public static String BIZKEY_TEACHER="TEACHER";
+	public static String BIZKEY_COURSE="COURSE";
+	public static String BIZKEY_ACTIVITY="ACTIVITY";
+	
+	public static String USER_TYPE_GENERAL="GENERAL";
+	public static String USER_TYPE_TEACHER="TEACHER";
+	public static String USER_TYPE_ORGANIZATION="ORGANIZATION";
+	
+	public static String TEACHER_ROLE_TEACHER="TEACHER";
+	public static String TEACHER_ROLE_ORGINIZER="ORGINIZER";
+	public static String TEACHER_ROLE_BOTH="BOTH";
+	
+	public static String CAREER_LENGTH_ZERO_TO_THREE="0";
+	public static String CAREER_LENGTH_THREE_TO_FIVE="1";
+	public static String CAREER_LENGTH_FIVE_TO_TEN="2";
+	public static String CAREER_LENGTH_MORE_THAN_TEN="3";
+	
+	public static String FAVORITE_INFO_TYPE_COURSE="COURSE";
+	public static String FAVORITE_INFO_TYPE_ACTIVITY="ACTIVITY";
+	
+	public static String PAY_STATUS_TOPAY="TOPAY";
+	public static String PAY_STATUS_PAYED="PAYED";
+}
