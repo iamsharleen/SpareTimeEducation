7834cc72cbcb4640c7bc1cb822c1ea3ae138bace
HeJianFeng <HeJianFeng@HeJF>
2016-04-30 18:40:28 +0800

    201604301839

--- spareTimeEdu/WebContent/app/js/config.router.js
+++ spareTimeEdu/WebContent/app/js/config.router.js
@@ -1,31 +1,28 @@
 var myModule = angular.module("app", ['ui.router']);
 
 myModule.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
-    $stateProvider
+    $urlRouterProvider.otherwise('/');     //匹配所有不在上面的路由
+	
+	$stateProvider
+        .state('home',{
+            url:'/',
+            template:'<div>index</div>',
+           controller:'appCtrl'   //指明控制器
+        })
         .state('course',{
             url:'/course',
-            template:'<div>index</div>',
-           controller:'myFormController'   //指明控制器
-        })
-        .state('course.list',{
-            url:'/list',
             templateUrl:'course/courseList.html'
         })
-        .state('course.detail',{
-            url:'/optional',
-            templateUrl:'form-optional.html'
+        .state('teacher',{
+            url:'/teacher',
+            templateUrl:'teacher/teacherList.html'
         })
-        .state('form.confirm',{
-            url:'/confirm',
-            templateUrl:'form-confirm.html'
-        });
-    
-        $urlRouterProvider.otherwise('/course/list');     //匹配所有不在上面的路由
+        
 }]);
 //定义模块的控制器
-myModule.controller('myFormController', ['$scope', function($scope){
-    $scope.formData = {};
-    $scope.submit = function() {
-        alert("Cool, you have registered!");
-    };
+myModule.controller('appCtrl', ['$scope', function($scope){
+//    $scope.formData = {};
+//    $scope.submit = function() {
+//        alert("Cool, you have registered!");
+//    };
 }]);
